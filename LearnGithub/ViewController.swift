123bd42e5b7326903c970431d728c2abadcb6839
Zaur <askerov.zaur@gmail.com>
2025-09-29 11:59:15 -0500

    newComponent

--- LearnGithub/ViewController.swift
+++ LearnGithub/ViewController.swift
@@ -8,13 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var lable: UILabel = {
+       let label = UILabel()
+        label.textColor = .white
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 30, weight: .bold)
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBlue
         title = "Learn Github"
     }
-
+    
+    
 
 }
 
